d54f6429b7334c484416d293dc8f28188dfd07ec
BaymaxShawn <95258216+BaymaxShawn@users.noreply.github.com>
2022-09-07 18:39:17 -0400

    Update and rename CS688_HW6_ShawnHu.py to Person_know_person.py

--- Person_know_person.py
+++ Person_know_person.py
@@ -31,7 +31,7 @@ for i in range(df.shape[0]):
 df = df.drop(droprows)
 df = df.reset_index(drop=True)
 
-#用networkx创建graph，networkx 是一个包，创建图用
+
 nlist = []
 elist = []
 g = nx.Graph()
@@ -53,14 +53,11 @@ startnode = np.sort(g.nodes())[0]
 endnode = np.sort(g.nodes())[-1]
 startnode = random.choice(np.sort(g.nodes))
 endnode = random.choice(np.sort(g.nodes)) 
-##可以 随机去取
 
 
-#question1 用两个算法找最短路径
-#找最短的路线 use dijkstra algorithm to generate the shortest path 
 d_path = nx.dijkstra_path(g,startnode,endnode)
 print(d_path)
-#可视化最短的路线 visualize the shortest path
+
 nodecolor = []
 for node in g.nodes():
     if node in d_path:
@@ -77,13 +74,12 @@ for edge in g.edges():
 nx.draw(g,with_labels = True,edge_color = edgecolor, node_color = nodecolor)
 
 
-#第二个方法
 #use A* algorithm to generate the shortest path
 a_path = nx.astar_path(g, startnode, endnode, heuristic=None, weight="weight")
 a_length = nx.astar_path_length(g, startnode, endnode, heuristic=None, weight="weight")
 print(a_path)
 print(a_length)
-#可视化最短的路线 visualize the shortest path
+
 nodecolor  = []
 for node in g.nodes():
     if node in a_path:
@@ -99,39 +95,28 @@ for edge in g.edges():
         edgecolor.append('grey')
 nx.draw(g,with_labels = True,edge_color = edgecolor, node_color = nodecolor)
 
-
-
-
-#question2 
-#第一个方式prim去找 MST， use Prim to find the MST
 primmst = tree.minimum_spanning_edges(g, algorithm='prim', data=False)
 primedgelist = list(primmst)
 print(primedgelist)
-#可视化新的图visualize the Prim MST
+
 prim_g = nx.Graph()
 prim_g.add_nodes_from(g.nodes)
 prim_g.add_edges_from(primedgelist)
 nx.draw(prim_g,with_labels = True, edge_color = 'grey', node_color = 'blue')
 
-#第二个方式K去找MST， use Kruskal to find the MST
 kruskalmst = tree.minimum_spanning_edges(g, algorithm='kruskal', data=False)
 kruskaledgelist = list(kruskalmst)
 print(kruskaledgelist)
-#可视化新的图visualize the Kruskal MST
+
 kruskal_g = nx.Graph()
 kruskal_g.add_nodes_from(g.nodes)
 kruskal_g.add_edges_from(kruskaledgelist)
 nx.draw(kruskal_g,with_labels = True, edge_color = 'grey', node_color = 'yellow')
 
 
-
-
-
-#question3
-#Page rank
 pr = nx.pagerank(g, alpha=0.4)
 print(pr)
-#用原点大小代表重要度，Use the size of the cricle to shows the importance of the node
+#Use the size of the cricle to shows the importance of the node
 nodesizepr = []
 for node in g.nodes():
     nodesize = 10000*pr[node]
@@ -157,7 +142,7 @@ for node in g.nodes():
     nodesizeh.append(nodesize)
 nx.draw(g,with_labels = True, edge_color = 'grey', node_color = 'green',node_size = nodesizeh)
 
-#用两个score的平均值来画node size的大小
+
 nodesizehits = []
 for node in g.nodes():
     nodesizehits.append(10000*(a[node]+h[node])/2)
@@ -167,15 +152,12 @@ nx.draw(g,with_labels = True, edge_color = 'grey', node_color = 'yellow',node_si
 
 
 
-
-#question4
-#用第一种方法 first one：Louvain for community detection
-#cluster的数量不是由自己规定的，算法会给出最优的类数，在这个题中有0到5，一共六类
+# first one：Louvain for community detection
 partition = community_louvain.best_partition(g)
 catagory = set([v for k,v in partition.items()])
 for i in catagory:
     print('For',i,'cluster(s), the nodes are',[k for k,v in partition.items() if v == i])
-#可视化看看结果visualize
+#visualize
 pos = nx.spring_layout(g)
 cmap = cm.get_cmap('summer', max(partition.values()) + 1)
 nx.draw_networkx_nodes(g, pos, partition.keys(), node_size=50,cmap=cmap, label = True, node_color=list(partition.values()))
